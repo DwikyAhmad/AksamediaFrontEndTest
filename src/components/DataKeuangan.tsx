424b89898b3133b0a56757d2a1d5fca431916502
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-01-27 00:27:54 +0700

    feat: added filter search by description

--- src/components/DataKeuangan.tsx
+++ src/components/DataKeuangan.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 interface Transaction {
     amount: number;
@@ -11,11 +12,16 @@ interface Transaction {
 }
 
 export default function DataKeuangan() {
+    const searchParams = useSearchParams();
+
     const router = useRouter();
     const [saldo, setSaldo] = useState<number>(0);
     const [listTransaction, setListTransaction] = useState<Transaction[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(1);
+    const [searchInput, setSearchInput] = useState<string>("");
     const itemsPerPage = 5;
+
+    const search = searchParams.get("description");
 
     useEffect(() => {
         const transactions = localStorage.getItem("transactions");
@@ -45,14 +51,28 @@ export default function DataKeuangan() {
         router.refresh();
     };
 
+    const filteredTransactions = (list: Transaction[]) => {
+        if (search !== null) {
+            return list.filter((transaction) =>
+                transaction.description
+                    .toLowerCase()
+                    .includes(search.toLowerCase())
+            );
+        }
+        return list;
+    };
+
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = listTransaction.slice(indexOfFirstItem, indexOfLastItem);
+    const currentItems = listTransaction.slice(
+        indexOfFirstItem,
+        indexOfLastItem
+    );
 
     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
     return (
-        <div className="font-poppins flex items-center justify-center flex-col gap-6">
+        <div className="font-poppins flex items-center justify-center flex-col gap-6 mb-6">
             <h1 className="mt-12 text-xl md:text-3xl font-semibold">
                 Saldo: Rp{saldo.toLocaleString("id-ID")}
             </h1>
@@ -63,23 +83,51 @@ export default function DataKeuangan() {
                 Tambah Transaksi
             </Link>
 
+            <div className="self-start px-4 sm:px-8 flex flex-wrap gap-4">
+                <input
+                    type="text"
+                    placeholder="Find by description..."
+                    className="text-black py-1 px-3 rounded-lg w-[280px]"
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    value={searchInput}
+                    onKeyDown={(e) => e.key === "Enter" && router.push(`/manage?description=${searchInput}`)}
+                />
+                <Link
+                    href={{
+                        pathname: "/manage",
+                        query: { description: searchInput },
+                    }}
+                    className="bg-slate-600 px-4 py-1 rounded-lg hover:bg-slate-700 duration-150"
+                >
+                    Search
+                </Link>
+            </div>
+
             <div className="flex overflow-x-auto w-full px-4 text-sm sm:px-8 sm:text-base">
                 <table className="table-auto mt-2 w-full">
                     <thead>
                         <tr>
                             <th className="border px-3 text-left py-2">No.</th>
-                            <th className="border px-3 text-left py-2">Category</th>
-                            <th className="border px-3 text-left py-2">Amount</th>
+                            <th className="border px-3 text-left py-2">
+                                Category
+                            </th>
+                            <th className="border px-3 text-left py-2">
+                                Amount
+                            </th>
                             <th className="border px-3 text-left py-2">
                                 Description
                             </th>
-                            <th className="border px-3 text-left py-2">Action</th>
+                            <th className="border px-3 text-left py-2">
+                                Action
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        {currentItems.map((transaction, index) => (
+                        {filteredTransactions(currentItems).map((transaction, index) => (
                             <tr key={index}>
-                                <td className="border px-3 py-2">{index + 1}</td>
+                                <td className="border px-3 py-2">
+                                    {index + 1}
+                                </td>
                                 <td className="border px-3 py-2">
                                     {transaction.category}
                                 </td>
@@ -90,7 +138,8 @@ export default function DataKeuangan() {
                                             : "text-red-500"
                                     }`}
                                 >
-                                    Rp{transaction.amount.toLocaleString("id-ID")}
+                                    Rp
+                                    {transaction.amount.toLocaleString("id-ID")}
                                 </td>
                                 <td className="border px-3 py-2">
                                     {transaction.description}
@@ -116,15 +165,26 @@ export default function DataKeuangan() {
                 </table>
             </div>
             <div className="flex justify-center mt-4">
-                {Array.from({ length: Math.ceil(listTransaction.length / itemsPerPage) }, (_, i) => (
-                    <button
-                        key={i}
-                        onClick={() => paginate(i + 1)}
-                        className={`mx-1 px-3 py-1 rounded-md ${currentPage === i + 1 ? 'bg-blue-600 text-white' : 'bg-gray-700'}`}
-                    >
-                        {i + 1}
-                    </button>
-                ))}
+                {Array.from(
+                    {
+                        length: Math.ceil(
+                            filteredTransactions(listTransaction).length / itemsPerPage
+                        ),
+                    },
+                    (_, i) => (
+                        <button
+                            key={i}
+                            onClick={() => paginate(i + 1)}
+                            className={`mx-1 px-3 py-1 rounded-md ${
+                                currentPage === i + 1
+                                    ? "bg-blue-600 text-white"
+                                    : "bg-gray-700"
+                            }`}
+                        >
+                            {i + 1}
+                        </button>
+                    )
+                )}
             </div>
         </div>
     );
